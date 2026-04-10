07a17d9a82c02b9fae22fba2b43bd249c495d1c9
TraumDE <eugene301workmaik@gmail.com>
2026-04-10 20:18:30 +0300

    Create base cli

--- vcpm.lua
+++ vcpm.lua
@@ -5,3 +5,7 @@ package.path = "./lua_modules/share/lua/" .. lua_version .. "/?.lua;" .. "./lua_
 package.path = "./lua_modules/lib/lua/" .. lua_version .. "/?.so;" .. package.path
 
 local argparse = require "argparse"
+
+local parser = argparse("vcpm", "VCPM - Voxel core project manager")
+
+local args = parser:parse()
